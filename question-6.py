032718fe344e78953259c92204e4690b94265e3d
Rakib-29p <mdpranta29@gmail.com>
2026-05-10 23:08:09 +0500

    Create question-6.py

---
+++ question-6.py
@@ -0,0 +1,6 @@
+def view_student_info(self):
+    
+    print("Student ID:", self.student_id)
+    print("Name:", self.name)
+    print("Department:", self.department)
+    print("Enrollment Status:", self.is_enrolled)
