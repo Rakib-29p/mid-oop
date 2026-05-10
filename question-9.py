3119df1d10636e919e68d19b21439fb0a667aca1
Rakib-29p <mdpranta29@gmail.com>
2026-05-10 23:12:51 +0500

    Create question-9.py

---
+++ question-9.py
@@ -0,0 +1,6 @@
+class Student:
+    def __init__(self, student_id, name, department):
+        self.__student_id = student_id
+        self.__name = name
+        self.__department = department
+        self.__is_enrolled = False
