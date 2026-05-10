48208a23076064ca4cc027e7e152080e2f3f06d6
Rakib-29p <mdpranta29@gmail.com>
2026-05-10 23:11:21 +0500

    Create question-7.py

---
+++ question-7.py
@@ -0,0 +1,16 @@
+while True:
+    print("1. View All Students")
+    print("2. Enroll Student")
+    print("3. Drop Student")
+    print("4. Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        pass
+    elif choice == "2":
+        pass
+    elif choice == "3":
+        pass
+    elif choice == "4":
+        break
+    else:
+        print("Invalid Choice")
